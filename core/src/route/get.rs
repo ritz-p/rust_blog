f4ae987eae63eaed7e1c120c930577b3757b60ea
ritz-p <okooko24816@gmail.com>
2025-10-25 04:12:48 +0900

    wip

--- core/src/route/get.rs
+++ core/src/route/get.rs
@@ -1,3 +1,4 @@
+pub mod about;
 pub mod article;
 pub mod category;
 pub mod error;
--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,7 +4,8 @@ mod m20250706_065150_create_article_table;
 mod m20250706_143055_create_tag_table;
 mod m20250706_144332_create_category_table;
 mod m20250706_145116_create_article_tag_table;
-mod m20250824_170452_create_article_category;
+mod m20250824_170452_create_article_category;mod m20251024_190826_create_fixed_table;
+
 
 pub struct Migrator;
 
