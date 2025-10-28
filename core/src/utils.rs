d2c31b8609159acf304fb21ab343401adc4f56e6
ritz-p <okooko24816@gmail.com>
2025-10-28 19:37:28 +0900

    fix: multi byte chars

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -4,5 +4,5 @@ pub mod markdown;
 
 pub fn cut_out_string(base: &str, limit: usize) -> String {
     let l = base.len().min(limit);
-    base[0..l].to_string()
+    base.chars().take(l).collect()
 }
