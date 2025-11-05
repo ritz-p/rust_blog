da40bff229a6da934dc65843a75ee86bdfb63818
ritz-p <okooko24816@gmail.com>
2025-11-05 16:59:42 +0900

    add: filter function

--- core/src/repository/article.rs
+++ core/src/repository/article.rs
@@ -12,6 +12,16 @@ pub async fn get_all_articles(db: &DatabaseConnection) -> Result<Vec<article::Mo
         .all(db)
         .await
 }
+
+pub async fn get_all_articles_with_updated_at(
+    db: &DatabaseConnection,
+) -> Result<Vec<article::Model>, DbErr> {
+    article::Entity::find()
+        .order_by(article::Column::CreatedAt, Order::Desc)
+        .all(db)
+        .await
+}
+
 pub async fn get_article_by_id(
     db: &DatabaseConnection,
     id: i32,
@@ -50,6 +60,27 @@ pub async fn get_articles_by_tag_slug(
     }
 }
 
+pub async fn get_articles_by_tag_slug_with_updated_at(
+    db: &DatabaseConnection,
+    tag_slug: &str,
+) -> Result<Vec<article::Model>, DbErr> {
+    if let Some(tag) = tag::Entity::find()
+        .filter(tag::Column::Slug.eq(tag_slug))
+        .one(db)
+        .await?
+    {
+        let articles: Vec<article::Model> = tag
+            .find_related(article::Entity)
+            .order_by_desc(article::Column::UpdatedAt)
+            .distinct()
+            .all(db)
+            .await?;
+        Ok(articles)
+    } else {
+        Err(DbErr::RecordNotFound("tag not found".into()))
+    }
+}
+
 pub async fn get_article_by_category_slug(
     db: &DatabaseConnection,
     category_slug: &str,
@@ -62,6 +93,27 @@ pub async fn get_article_by_category_slug(
         let articles: Vec<article::Model> = category
             .find_related(article::Entity)
             .order_by_desc(article::Column::CreatedAt)
+            .distinct()
+            .all(db)
+            .await?;
+        Ok(articles)
+    } else {
+        Err(DbErr::RecordNotFound("category not found".into()))
+    }
+}
+
+pub async fn get_article_by_category_slug_with_updated_at(
+    db: &DatabaseConnection,
+    category_slug: &str,
+) -> Result<Vec<article::Model>, DbErr> {
+    if let Some(category) = category::Entity::find()
+        .filter(category::Column::Slug.eq(category_slug))
+        .one(db)
+        .await?
+    {
+        let articles: Vec<article::Model> = category
+            .find_related(article::Entity)
+            .order_by_desc(article::Column::UpdatedAt)
             .distinct()
             .all(db)
             .await?;
