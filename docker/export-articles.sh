3bd303bc9bd1268124a0401343cbf5f3bfc84c15
ritz-p <okooko24816@gmail.com>
2026-09-24 22:05:55 +0900

    fix: derive static export update dates from Markdown history

--- docker/export-articles.sh
+++ docker/export-articles.sh
@@ -7,6 +7,7 @@ export DATABASE_URL="sqlite://$export_db_dir/blog.db?mode=rwc"
 export CONFIG_TOML_PATH="${RUST_BLOG_CONFIG_PATH:-${CONFIG_TOML_PATH:-blog_config.toml}}"
 export RUST_BLOG_CONFIG_PATH="$CONFIG_TOML_PATH"
 export RUST_BLOG_REQUIRE_CREATED_AT=1
+export RUST_BLOG_FILE_TIMESTAMPS=1
 
 cargo run --locked -p migration --bin migration -- up
 cargo run --locked -p rust_blog --bin seed
